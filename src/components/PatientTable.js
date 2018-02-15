b459a323ddbf05be72ca518ee5f47fc5214f8d23
Jerry Walters <jerrydwalters@gmail.com>
2018-02-15 15:04:04 -0800

    refactor PatientTable to use PatientRow component

--- src/components/PatientTable.js
+++ src/components/PatientTable.js
@@ -1,15 +1,38 @@
 import React from 'react';
 import patients from '../patients.json';
 
-const PatientTable = () => {
-    const patientList = patients.map((patient) => 
+// const PatientTable = () => {
+//     const patientList = patients.map((patient) => 
+//         <tr>
+//             <td>{`${patient.name.last}, ${patient.name.first}`}</td>
+//             <td>{`${patient.mrn}`}</td>
+//             <td>{`${patient.dob}`}</td>
+//             <td>{`${patient.sex}`}</td>
+//             <td>{`${patient.treatment_site}`}</td>
+//         </tr>
+//     );
+
+const PatientRow = (props) => {
+    return (
         <tr>
-            <td>{`${patient.name.last}, ${patient.name.first}`}</td>
-            <td>{`${patient.mrn}`}</td>
-            <td>{`${patient.dob}`}</td>
-            <td>{`${patient.sex}`}</td>
-            <td>{`${patient.treatment_site}`}</td>
+            <td>{`${props.firstName}, ${props.lastName}`}</td>
+            <td>{`${props.mrn}`}</td>
+            <td>{`${props.dob}`}</td>
+            <td>{`${props.sex}`}</td>
+            <td>{`${props.treatmentSite}`}</td>
         </tr>
+    )
+}
+
+const PatientTable = (props) => {
+    const patientList = patients.map((patient) => 
+        <PatientRow 
+            firstName = {patient.name.first}
+            lastName = {patient.name.last}
+            mrn = {patient.mrn}
+            dob = {patient.dob}
+            sex = {patient.sex}
+            treatmentSite = {patient.treatment_site} />
     );
 
     return (
