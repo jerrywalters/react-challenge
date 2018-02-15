eac977fbdff58b1681a43c2b2ee3138bcf1bcc79
Jerry Walters <jerrydwalters@gmail.com>
2018-02-15 15:09:00 -0800

    broke PatientRow out into its own functional component

--- src/components/PatientRow.js
+++ src/components/PatientRow.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import patients from '../patients.json';
 
-const PatientRow = () => {
+const PatientRow = (props) => {
     return (
-        <div>
-            this will contain an individual patient row
-        </div>
+        <tr>
+            <td>{`${props.firstName}, ${props.lastName}`}</td>
+            <td>{`${props.mrn}`}</td>
+            <td>{`${props.dob}`}</td>
+            <td>{`${props.sex}`}</td>
+            <td>{`${props.treatmentSite}`}</td>
+        </tr>
     )
 }
 
-export default PatientTable;
+export default PatientRow;
--- src/components/PatientTable.js
+++ src/components/PatientTable.js
@@ -1,28 +1,7 @@
 import React from 'react';
 import patients from '../patients.json';
 
-// const PatientTable = () => {
-//     const patientList = patients.map((patient) => 
-//         <tr>
-//             <td>{`${patient.name.last}, ${patient.name.first}`}</td>
-//             <td>{`${patient.mrn}`}</td>
-//             <td>{`${patient.dob}`}</td>
-//             <td>{`${patient.sex}`}</td>
-//             <td>{`${patient.treatment_site}`}</td>
-//         </tr>
-//     );
-
-const PatientRow = (props) => {
-    return (
-        <tr>
-            <td>{`${props.firstName}, ${props.lastName}`}</td>
-            <td>{`${props.mrn}`}</td>
-            <td>{`${props.dob}`}</td>
-            <td>{`${props.sex}`}</td>
-            <td>{`${props.treatmentSite}`}</td>
-        </tr>
-    )
-}
+import PatientRow from './PatientRow';
 
 const PatientTable = (props) => {
     const patientList = patients.map((patient) => 
