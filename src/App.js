f8782dfce07ea2132c2f6127db708ce07e0c4447
Jerry Walters <jerrydwalters@gmail.com>
2018-02-15 15:31:28 -0800

    bring in react-router

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+
 import PatientTable from './components/PatientTable';
 
 class App extends Component {
--- src/components/PatientTable.js
+++ src/components/PatientTable.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import patients from '../patients.json';
 
+import patients from '../patients.json';
 import PatientRow from './PatientRow';
 
 const PatientTable = (props) => {
