7c372e80380c3dc81fe874dbc162db8fc8e54a83
Jerry Walters <jerrydwalters@gmail.com>
2018-02-15 22:46:35 -0800

    find the lowest, highest, and median patient weights

--- src/components/PatientDetail.js
+++ src/components/PatientDetail.js
@@ -22,6 +22,33 @@ const PatientSingle = (props) => {
     // use moment.js to get age from dob
     const age = moment().diff(dob, 'years');
 
+    // use .reduce() to find the lowest and highest weights in the array of patients
+    const lowestWeight = patients.reduce((prev, current) => prev.weight < current.weight ? prev:current).weight;
+    const highestWeight = patients.reduce((prev, current) => prev.weight > current.weight ? prev:current).weight;
+    const medianWeight = median(patients);
+
+    // another way to find the lowest or highest, decided against because it iterates thru array twice
+    // const lowestWeight = patients
+    //     .map(patient => patient.weight)
+    //     .sort((a, b)=> a-b)[0];
+
+    function median(values) {
+        // get all patients weights and put in numberical order
+        const allWeights = values
+            .map(values => values.weight)
+            .sort((a, b)=> a-b);
+        
+        // get index of middle number in array
+        const half = Math.floor(allWeights.length/2);
+    
+        // if the array has an even number of indexes, then take the mean of the middle 2
+        // if the array has an odd number of indexes, then take the middle number
+        if(allWeights.length % 2)
+            return (allWeights[half-1] + allWeights[half]) / 2.0;
+        else
+            return allWeights[half];
+    }
+
     // if the patient's mrn matches the current route param, return true
     function isSelectedPatient(patient) {
         return patient.mrn === patientId;
